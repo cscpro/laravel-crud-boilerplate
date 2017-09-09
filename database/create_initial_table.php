9590ae3d17b7820e17d3c2eb2995ce0eb03919d2
Harun R <harunr@cscpro.org>
2017-09-09 07:10:48 +0700

    Create create_initial_table.php

---
+++ database/create_initial_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInitialTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+		// Add column on existing table
+		Schema::table( 'users', function( Blueprint $table ) {
+			$table->string( 'nip' )->after( 'name' )->nullable();
+		} );
+
+		// Create new table
+		Schema::create( 'seasons', function ( Blueprint $table ) {
+			$table->increments( 'id' );
+			$table->integer( 'year' );
+			$table->boolean( 'is_current' )->default( false );
+			$table->timestamps();
+		} );
+
+		// Create table that has foreign key
+		Schema::create( 'periods', function ( Blueprint $table ) {
+			$table->increments( 'id' );
+			$table->integer( 'season_id' )->unsigned();
+			$table->integer( 'role' );
+			$table->string( 'date_from' );
+			$table->string( 'date_to' );
+			$table->timestamps();
+
+			$table->unique( ['season_id', 'role'] );
+			$table->foreign( 'season_id' )
+				->references( 'id' )
+				->on( 'seasons' )
+				->onDelete( 'cascade' )
+			;
+		} );
+
+	}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		Schema::drop( 'periods' );
+		Schema::drop( 'seasons' );
+		Schema::table( 'users', function( Blueprint $table ) {
+			$table->dropColumn( 'nip' );
+		} );
+    }
+
+}
